4866ff515248219a686f84ea8efac79c07dc71d9
Sankar Annamalai <sankar0594@gmail.com>
2026-05-06 01:13:11 +0530

    Fix deployment

--- app.py
+++ app.py
@@ -249,4 +249,6 @@ def handle_get_rare_bosses():
     emit('rare_bosses_list', {'bosses': RARE_BOSSES})
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    socketio.run(app, host='0.0.0.0', port=port, debug=True, allow_unsafe_werkzeug=True)
